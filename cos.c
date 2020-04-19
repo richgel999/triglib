9a995805f5d104c8237579e73df02b7051db5550
Rich Geldreich <richgel99@gmail.com>
2020-04-19 01:32:19 -0400

    Fixed CRLF

--- cos.c
+++ cos.c
@@ -1,1 +1,1 @@
-/* cos(x) - cosine of a real number.          argument x must be a double          returns a double in radians */#include <trigcons.h>double cos(x)double x;{     double _sincos();     reduce(&x,TWO_PI);     return(_sincos(0.25 - x));}
+/* cos(x) - cosine of a real number.          argument x must be a double          returns a double in radians */#include <trigcons.h>double cos(x)double x;{     double _sincos();     reduce(&x,TWO_PI);     return(_sincos(0.25 - x));}
